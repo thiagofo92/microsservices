c146b8d0a8e5c045568760690ea5170d69910a25
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-05-31 19:05:07 -0300

    Add to use the service injection

--- api/request-payment/src/core/handler/payment-bill.handler.ts
+++ api/request-payment/src/core/handler/payment-bill.handler.ts
@@ -1,8 +1,11 @@
+import { PaymentServicePort } from "src/infra/port";
 import { Handler } from "../port/handler";
 
 export class PaymentBillHandler implements Handler {
+  constructor(private readonly service: PaymentServicePort) {}
   async execute <R>(input: any): Promise<R> {
-    return '' as R
+    const result = await this.service.generatePaymentBill(input)
+    return result as R
   }
 
 }
