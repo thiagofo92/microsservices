2a9995b6050505198e090f3d4f1e2ff1cf5d949b
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-05-31 19:04:44 -0300

    Update

--- api/request-payment/src/core/__test__/credit.handler.spec.ts
+++ api/request-payment/src/core/__test__/credit.handler.spec.ts
@@ -1,14 +1,12 @@
 import { QueueService } from "src/infra/service";
 import { describe, expect, it, vi  } from "vitest";
 import { CreditHandler } from "../handler";
-import 'axios'
 
 describe('# CreditHandler', () => {
   it('Sucess execute', async () => {
     const service = new QueueService()
-    const axiosMock = vi.mocked('axios')
 
-    console.log(axiosMock)
+    // console.log('Axios',postMock)
     const handler = new CreditHandler(service)
 
     const result = await handler.execute<void>('')
